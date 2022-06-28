1e7fb98dddc9c274069d12653f2e5cc4ed294ce0
Dinoxaur <aguspray001@gmail.com>
2022-06-28 22:03:18 +0700

    feat: add seo

---
+++ src/constant/index.js
@@ -0,0 +1,4 @@
+export const BASE_PATH = "https://solaris-fe.vercel.app";
+export const BRAND_TITLE = "Solaris Travel";
+export const BRAND_LONG_DESC =
+  "situs ini bertujuan untuk memberikan informasi dan mempermudah customer untuk melakukan persewaan bis pariwisata kami";
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,31 @@
-import '../../styles/tailwind.css'
+import { DefaultSeo } from "next-seo";
+import "../../styles/tailwind.css";
+import { BASE_PATH, BRAND_LONG_DESC, BRAND_TITLE } from "../constant";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <>
+      <DefaultSeo
+        canonical={`${BASE_PATH}${router.asPath || "/"}`}
+        description={BRAND_LONG_DESC}
+        openGraph={{
+          type: "website",
+          title: BRAND_TITLE,
+          description: BRAND_LONG_DESC,
+          locale: "id_ID",
+          site_name: BRAND_TITLE,
+        }}
+        title={BRAND_TITLE}
+        titleTemplate={`%s | ${BRAND_TITLE}`}
+        twitter={{
+          handle: "@aguspray6",
+          site: "@aguspray6",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
